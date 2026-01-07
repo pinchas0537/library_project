cd3932d64c44b6e02127470496069878032f3cbc
Haim Vales <haimvales@gmail.com>
2026-01-07 16:57:43 +0200

    deleteNote

--- services/notesS.js
+++ services/notesS.js
@@ -1,12 +1,26 @@
-import {listByOwner} from "../DAL/notesD.js"
-export async function listNotes(ownerUsername){
-    try{
-        if(typeof(ownerUsername)===String){
-            const listNotes_by_username=listByOwner(ownerUsername)
+import { deleteNotes, existsNote, listByOwner } from "../DAL/notesD.js"
+import { existsUser } from "../DAL/usersD.js";
+export async function listNotes(ownerUsername) {
+    try {
+        if (typeof (ownerUsername) === String) {
+            const listNotes_by_username = listByOwner(ownerUsername)
             return listNotes_by_username
-    }else{
-        console.log("The ownerUsername not string.")
-    }}catch{error}{
+        } else {
+            console.log("The ownerUsername not string.")
+        }
+    } catch { error } {
         console.error(error)
+    }
+}
+
+export function deleteNote(ownerUsername, noteId) {
+    try {
+        if (existsUser(ownerUsername) && existsNote(noteId)) {
+            deleteNotes(ownerUsername, noteId)
+            return true
+        }
+        return false
+    } catch (err) {
+        console.log(err)
     }
 }
