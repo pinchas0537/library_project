2e1c86aac5b3cfade60a784a793ee4c7707e3a29
Haim Vales <haimvales@gmail.com>
2026-01-07 15:06:21 +0200

    listByOwner

--- DAL/notesD.js
+++ DAL/notesD.js
@@ -1,15 +1,28 @@
 import notesDB from "../db/notesDB.js"
-export async function deleteNotes(id,username){
-    try{for(let i= 0;i<notesDB.length;i++){
-        if(notesDB[i].id===id&&notesDB[i].username===username){
-            notesDB.splice(i,1)
-            console.log("Deleted successfully")
+export async function deleteNotes(id, username) {
+    try {
+        for (let i = 0; i < notesDB.length; i++) {
+            if (notesDB[i].id === id && notesDB[i].username === username) {
+                notesDB.splice(i, 1)
+                console.log("Deleted successfully")
+            }
         }
+        return notesDB
     }
-    return notesDB}
-    catch{
+    catch {
         console.error(error);
-        
+
+    }
+}
+
+export async function listByOwner(username) {
+    try {
+        const ListOfNotes = notesDB.forEach(element => {
+            element.username === username;
+        });
+        return ListOfNotes;
+    } catch (err) {
+        return a.message;
     }
 }
 
