7597fc1f6e368f4b2f91b41ba71e5ba3b5715828
yaacov elchanan kahn <ek0533180287@gmail.com>
2026-01-07 14:50:29 +0200

    deleteNotes

--- DAL/notesD.js
+++ DAL/notesD.js
@@ -3,6 +3,7 @@ export async function deleteNotes(id,username){
     try{for(let i= 0;i<notesDB.length;i++){
         if(notesDB[i].id===id&&notesDB[i].username===username){
             notesDB.splice(i,1)
+            console.log("Deleted successfully")
         }
     }
     return notesDB}
