1a3d9b93e4e7d56292777b42c9104ded49ad1673
Haim Vales <haimvales@gmail.com>
2026-01-07 15:21:34 +0200

    addUser

--- DAL/notesD.js
+++ DAL/notesD.js
@@ -1,15 +1,27 @@
-import notesDB from "../db/notesDB.js"
-export async function deleteNotes(id,username){
-    try{for(let i= 0;i<notesDB.length;i++){
-        if(notesDB[i].id===id&&notesDB[i].username===username){
-            notesDB.splice(i,1)
-            console.log("Deleted successfully")
+import notesDB from "../db/notesDB.js";
+import usersDB from "../db/usersDB.js";
+
+export async function deleteNotes(id, username) {
+    try {
+        for (let i = 0; i < notesDB.length; i++) {
+            if (notesDB[i].id === id && notesDB[i].username === username) {
+                notesDB.splice(i, 1)
+                console.log("Deleted successfully")
+            }
         }
+        return notesDB
     }
-    return notesDB}
-    catch{
+    catch {
         console.error(error);
-        
+
     }
 }
 
+export async function add(user) {
+    try {
+        usersDB.push(user)
+        return { msg: "success" }
+    } catch (err) {
+        return { err: err.nessage }
+    }
+}
