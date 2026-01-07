fe9a1c50837a752a919cd054d196977ecbd4bede
Ariel_Naiman <a0527633600@gmail.com>
2026-01-07 15:29:37 +0200

    Function to add a comment to the DB

--- DAL/notesD.js
+++ DAL/notesD.js
@@ -1,15 +1,25 @@
 import notesDB from "../db/notesDB.js"
-export async function deleteNotes(id,username){
-    try{for(let i= 0;i<notesDB.length;i++){
-        if(notesDB[i].id===id&&notesDB[i].username===username){
-            notesDB.splice(i,1)
-            console.log("Deleted successfully")
+
+export async function deleteNotes(id, username) {
+    try {
+        for (let i = 0; i < notesDB.length; i++) {
+            if (notesDB[i].id === id && notesDB[i].username === username) {
+                notesDB.splice(i, 1)
+                console.log("Deleted successfully")
+            }
         }
+        return notesDB
     }
-    return notesDB}
-    catch{
+    catch (error) {
         console.error(error);
-        
     }
 }
 
+export function add(note = {}) {
+    try {
+        notesDB.push(note)
+        return true
+    } catch (error) {
+        console.error(error)
+    }
+}
